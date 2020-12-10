30912339421d9fe1e3df481ca48b7007b0b6c2a2
Alexey Zadunaiskiy <alexey.zdn@gmail.com>
2020-12-11 01:00:44 +0200

    Accidental wrong revert fix

--- js/pageinteraction.js
+++ js/pageinteraction.js
@@ -44,3 +44,7 @@ function changeColorValuesToDataFromSelector(){
     legendcolors[i].childNodes[1].nodeValue = range[index].value + '' + (next >= 0 ? (' - ' + range[next].value) : ' +');
   }
 }
+
+function updateDataEvent() {
+  updateDataLayer(false);
+}
